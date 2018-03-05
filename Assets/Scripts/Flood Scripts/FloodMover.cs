62c93222408a8e24e778cf269f9205d72bf36fd8
Harry <harry_talbot@outlook.com>
2018-03-05 04:08:49 +0000

    added code to stop the flood moving forever when you die
     - now covers the whole screen and then stops

--- Assets/Scripts/Flood Scripts/FloodMover.cs
+++ Assets/Scripts/Flood Scripts/FloodMover.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloodMover : MonoBehaviour {
+public class FloodMover : MonoBehaviour
+{
 
-    public float floodSpeed, delay;
+    public float floodSpeed, delay, yMax;
+
     private float startTime;
     private Vector3 startPos, distMoved;
     private FloodMover floodMover;
@@ -27,20 +29,24 @@ public class FloodMover : MonoBehaviour {
         // first, wait for the delay so the wave movement is not instant
         elapsedTime += Time.deltaTime;
         // once enough time has passed...
-        if (elapsedTime > delay) {
+        if (elapsedTime > delay)
+        {
             // and the wave should rise 
-            if (shouldRise)
+            if (shouldRise && (Time.time > waitTime))
             {
-                if (Time.time > waitTime)
-                {
-                    Vector3 v = transform.position;
-                    // Multiply with delta time to give movement smoothly with framerate
-                    v.y += (floodSpeed * Time.deltaTime);
+
+                Vector3 v = transform.position;
+                // Multiply with delta time to give movement smoothly with framerate
+                v.y += (floodSpeed * Time.deltaTime);
+                // get a position to make sure the flood stops when it covers the whole screen
+                // basically want it to keep going when you die but not forever as it'll go off the screen
+                yMax = GameObject.FindWithTag("MainCamera").transform.position.y + 6;
+                if (v.y < yMax)
                     transform.position = v;
-                }
             }
+
         }
-        
+
     }
 
 }
